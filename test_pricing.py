7a69e2d575addc18ef531cbcac57b728c2dc55b0
Avi Mukesh <avimukesh10@gmail.com>
2026-07-04 11:53:55 +0100

    added validation for the terminal price from working out the full price paths

--- test_pricing.py
+++ test_pricing.py
@@ -5,18 +5,25 @@ K = 100
 T = 1
 rf = 0.05
 sigma = 0.2
+iterations = 100000
 
 params = MarketParams(S0, K, T, rf, sigma)
 
-mc_pricer = MonteCarloPricer(params, 10000000)
+mc_pricer = MonteCarloPricer(params, iterations)
 mc_pricer.simulate_terminal_prices()
 
 mc_call, std_error = mc_pricer.call_price()
 bs_call = bs_call_price(params)
+print('mc call price {:.3f}'.format(mc_call))
+print('bs call price {:.3f}'.format(bs_call))
+print('standard error {:.4f}\n'.format(std_error))
 assert(abs(mc_call - bs_call) < 2 * std_error) # will fail about 5% of the time
 
 mc_put, std_error = mc_pricer.put_price()
 bs_put = bs_put_price(params)
+print('mc put price {:.3f}'.format(mc_put))
+print('bs put price {:.3f}'.format(bs_put))
+print('standard error {:.4f}\n'.format(std_error))
 assert(abs(mc_put - bs_put) < 2 * std_error)
 
 # put-call parity holds for BS (should be exact)
@@ -27,5 +34,15 @@ discounted_terminal_prices = np.exp(-rf * T) * mc_pricer.terminal_prices
 std_error_pc_parity = np.std(discounted_terminal_prices) / np.sqrt(mc_pricer.iterations)
 assert(abs(mc_call - mc_put - S0 + K * np.exp(-rf * T)) < 2 * std_error_pc_parity)
 
-
+# ensuring that the method that simulated the full price paths (instead of just the terminal prices) is valid
 mc_pricer.simulate_price_paths(252)
+price_paths = mc_pricer.price_simulations
+terminal = np.array(price_paths)[:, -1]
+mc_call_prices = np.exp(-rf*T)*np.maximum(terminal - K, 0)
+mc_call_2 = np.mean(mc_call_prices)
+std_error = np.std(mc_call_prices) / np.sqrt(iterations)
+
+print('mc call price (full path simulation) {:.3f}'.format(mc_call_2))
+print('bs call price {:.3f}'.format(bs_put))
+print('standard error {:.4f}'.format(std_error))
+assert(abs(mc_call_2 - bs_call) < 2 * std_error)
